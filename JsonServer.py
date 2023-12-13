dac6b68402e5c16a4bde0a03e16d102c1f6b4afa
Trifun Savic <trifunsavic@gmail.com>
2023-12-13 12:01:58 +0100

    Adding the default AIOT Manager serial port /dev/ttyS0 for JsonServer to connect

--- JsonServer.py
+++ JsonServer.py
@@ -500,7 +500,7 @@ if __name__=="__main__":
     parser.add_argument('--tcpport',                    default=8080)
     parser.add_argument('--autoaddmgr',   type=str2bool,default=False)
     parser.add_argument('--autodeletemgr',type=str2bool,default=False)
-    parser.add_argument('--serialport',                 default=None)
+    parser.add_argument('--serialport',                 default='/dev/ttyS0')
     parser.add_argument('--configfilename',             default='JsonServer.config')
     parser.add_argument('--allowremote',  type=str2bool,default=False)
     args = vars(parser.parse_args())
