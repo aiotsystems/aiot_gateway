3d02f27f23cf4d9b6dad3a4126f9f8e46c2e821a
Thomas Watteyne <twatteyne@gmail.com>
2024-04-09 14:58:36 +0200

    #4. Mote colors work.

--- smartmeshsdk3/app/InriaMuseum/static/museum.js
+++ smartmeshsdk3/app/InriaMuseum/static/museum.js
@@ -1,10 +1,6 @@
 var uiReady = false;
 
-//=========================== defines =========================================
-
-//=========================== globals =========================================
-
-//=========================== helpers =========================================
+//=========================== initialization ==================================
 
 function loadSvg() {
     d3.xml('/static/museum.svg')
@@ -17,4 +13,29 @@ function loadSvg() {
 function buildPage() {
     
     uiReady = true;
+}
+
+//=========================== periodic ========================================
+
+function getData() {
+    
+    $.getJSON('museum.json', function(data) {
+        updateUI(data);
+    })
+}
+
+function updateUI(data) {
+    
+    // abort if UI not ready
+    if (uiReady==false) {
+        return;
+    }
+    
+    // motes
+    for (const [key,value] of Object.entries(data.motes)) {
+        d3.select('#'+key)
+            .style("fill",         value.fill)
+            .style("stroke",       value.stroke)
+            .style("stroke-width", 3);
+    }
 }
