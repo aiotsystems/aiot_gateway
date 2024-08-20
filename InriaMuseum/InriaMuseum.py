63e4b8772c605bc836500a0919a526ad89e9d0c5
Riviere ChloeMarieLucie <chloe-marie-lucie.riviere@telecomnancy.eu>
2024-08-20 17:12:19 +0400

    adding interation with Raspberry Pi

--- InriaMuseum/InriaMuseum.py
+++ InriaMuseum/InriaMuseum.py
@@ -1,11 +1,58 @@
-from bottle import route, run, static_file
+from bottle import route, run, static_file, Bottle, template
+import requests
+import json
 
-@route('/')
+SERVERtoLISTEN = "http://localhost:8081/json"
+messages = []  
+app = Bottle()
+table = None
+with open('table.json', 'r') as file:
+    table = json.load(file)
+    
+@app.route('/')
 def index():
-    return static_file('InriaMuseum.html', root='.')
+     return template('InriaMuseum.html')
+    
+def Get_position(json):
+    mac_addr= None
+    if json['name'] == "notifData" : 
+        json = json["fields"]
+        mac_addr = json["macAddress"]
+        data= table.get(mac_addr)
+        if json['data'] == [0]:
+            data['isSomeone'] = False
+        else :
+            data['isSomeone'] = True
+        print("MAC addr : ",mac_addr)
+        print("Data to send : ",data)
+        return data
+    else:
+        return None
+    
+def Get_Mote_Data():
+    try:
+        response = requests.get(SERVERtoLISTEN)
+        if response.status_code == 200:
+            json_data = response.json()  
+            print(f"Message receive : {json_data}")
+            json= json_data.get("payload")
+            data = Get_position(json)
+            return data
+        else:
+            print("Error : recieve")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error : connection  {e}")
+        return None
 
-@route('/<filename:path>')
+@app.route('/api/message')
+def get_message():
+    data = Get_Mote_Data()
+    return {"message": data}
+
+@app.route('/<filename:path>')
 def serve_static(filename):
     return static_file(filename, root='.')
 
-run(host='localhost', port=8080)
+if __name__ == '__main__':
+    run(app, host='localhost', port=8080)
